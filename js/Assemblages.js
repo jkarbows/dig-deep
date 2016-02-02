af953a3f4677e0da4f0132155944ddb853a324d9
Jeremy <jkarbows@gmail.com>
2016-02-01 23:26:45 -0500

    next up we get it running and make working collision. go bernie go

---
+++ js/Assemblages.js
@@ -0,0 +1,11 @@
+// Assemblages are like entity templates
+// Each assemblage returns an entity like a factory or w/e
+// There may be a better way to go about structuring these things
+
+DIG.Assemblages = {
+    Assemblage: function Assemblage(){
+        var entity = new DIG.Entity();
+        entity.addComponent( new DIG.Components.componentName());
+        return entity;
+    }
+};
